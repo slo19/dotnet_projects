fc0052d91847be5e66b8e75b1e165f0c70deca2e
saulo-miranda <saulo.miranda@ufv.br>
2024-02-14 23:12:40 -0300

    productsummary added

--- SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -20,6 +20,11 @@ namespace SportsStore.Infrastructure {
 
         public PagingInfo? PageModel {get; set;}
         public string? PageAction {get; set;}
+
+        public bool PageClassesEnabled {get; set;} = false;
+        public string PageClass { get;set;} = String.Empty;
+        public string PageClassNormal { get; set;} = String.Empty;
+        public string PageClassSelected {get; set;} = String.Empty; 
         public override void Process(TagHelperContext context, TagHelperOutput output){
             if(ViewContext != null && PageModel != null) {
                 IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
@@ -27,6 +32,10 @@ namespace SportsStore.Infrastructure {
                 for (int i = 1; i <= PageModel.TotalPages; i++) {
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i});
+                    if (PageClassesEnabled) {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
                     tag.InnerHtml.Append(i.ToString());
                     result.InnerHtml.AppendHtml(tag);
                 }
