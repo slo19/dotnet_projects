5df0e4d883d324136da1f7afdd2a7ff32c32868c
saulo-miranda <saulo.miranda@ufv.br>
2024-02-25 14:15:48 -0300

    short circuiting middleware

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -5,7 +5,17 @@ app.Use(async (context, next) => {
     await next();
     await context.Response
         .WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
-        });
+});
+
+app.Use(async (context, next) => {
+    if (context.Request.Path == "/short") {
+        await context.Response
+            .WriteAsync($"Request Short Circuited");
+    } else {
+        await next();
+    }
+});
+
 app.Use(async (context, next) => {
     if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true") {
       context.Response.ContentType = "text/plain";
