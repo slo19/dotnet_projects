a89e264c849f8fe6526d41e30f4c3c14d2301648
saulo-miranda <saulo.miranda@ufv.br>
2024-02-28 21:35:20 -0300

    using options to configure middleware

---
+++ Platform/MessageOptions.cs
@@ -0,0 +1,7 @@
+namespace Platform {
+
+  public class MessageOptions {
+    public string CityName { get; set; } = "Belo Horizonte";
+    public string CountryName { get; set; } = "Brazil";
+  }
+}
--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,32 +1,19 @@
+using Microsoft.Extensions.Options;
+using Platform;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MessageOptions>(options => {
+    options.CityName = "Betim";
+});
+
 var app = builder.Build();
 
-((IApplicationBuilder)app).Map("/branch", branch => {
-    branch.Run(new Platform.QueryStringMiddleWare().Invoke);
+app.MapGet("/location", async (HttpContext context,
+    IOptions<MessageOptions> msgOpts) => {
+    Platform.MessageOptions opts = msgOpts.Value;
+    await context.Response.WriteAsync($"{opts.CityName}, {opts.CountryName}");
 });
 
-app.Use(async (context, next) => {
-    await next();
-    await context.Response
-        .WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
-});
-
-app.Use(async (context, next) => {
-    if (context.Request.Path == "/short") {
-        await context.Response
-            .WriteAsync($"Request Short Circuited");
-    } else {
-        await next();
-    }
-});
-
-app.Use(async (context, next) => {
-    if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true") {
-      context.Response.ContentType = "text/plain";
-      await context.Response.WriteAsync("Custom Middleware \n");
-    }
-    await next();
-});
 app.MapGet("/", () => "Hello World!");
-app.UseMiddleware<Platform.QueryStringMiddleWare>();
 app.Run();
