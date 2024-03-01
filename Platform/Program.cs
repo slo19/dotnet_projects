17965c9ba720824fb93a11c366c87d05bb830d34
saulo-miranda <saulo.miranda@ufv.br>
2024-03-01 13:39:36 -0300

    routing +middleware

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -3,8 +3,19 @@ using Platform;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseMiddleware<Population>();
-app.UseMiddleware<Capital>();
+//app.UseMiddleware<Population>();
+//app.UseMiddleware<Capital>();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints => {
+    endpoints.MapGet("routing", async context => {
+        await context.Response.WriteAsync("Request Was Routed");
+    });
+    endpoints.MapGet("capital/egypt", new Capital().Invoke);
+    endpoints.MapGet("population/paris", new Population().Invoke);
+});
+
 app.Run(async (context) => {
    await context.Response.WriteAsync("Terminal Middleware Reached"); 
 });
