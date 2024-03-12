a6919caf666d9d9f6d8f72837c5a5a3d3438aa01
saulo-miranda <saulo.miranda@ufv.br>
2024-03-11 21:43:05 -0300

    singleton and typebroker patterns

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -6,15 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseMiddleware<WeatherMiddleware>();
 
-IResponseFormatter formatter = new TextResponseFormatter();
+//IResponseFormatter formatter = new TextResponseFormatter();
 app.MapGet("middleware/function", async (context) => {
-    await formatter.Format(context, "Middleware Function: It is snowing in Chicago");
+    await TypeBroker.Formatter
+    .Format(context, "Middleware Function: It is snowing in Chicago");
 });
 
 app.MapGet("endpoint/class", WeatherEndpoint.Endpoint);
 
 app.MapGet("endpoint/function", async context => {
-    await context.Response.WriteAsync("Endpoint Function: It is sunny in LA");
+    await TypeBroker.Formatter
+    .Format(context, "Endpoint Function: It is sunny in LA");
 });
 
 app.Run();
