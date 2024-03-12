286018323e3e7d23ad51574be4a3b2f522dfbc84
saulo-miranda <saulo.miranda@ufv.br>
2024-03-11 23:05:31 -0300

    guidservice added

---
+++ Platform/Services/GuidService.cs
@@ -0,0 +1,10 @@
+namespace Platform.Services
+{
+    public class GuidService : IResponseFormatter {
+      private Guid guid = Guid.NewGuid();
+
+      public async Task Format(HttpContext context, string content) {
+        await context.Response.WriteAsync($"Guid: {guid}\n{content}");
+      }
+    }
+}
