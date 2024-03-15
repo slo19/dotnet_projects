d8ff4ec86beeeb1f1bbb9200e1415b4c8afbd656
saulo-miranda <saulo.miranda@ufv.br>
2024-03-14 23:42:30 -0300

    avoiding transient reuse

--- Platform/Services/EndpointExtensions.cs
+++ Platform/Services/EndpointExtensions.cs
@@ -12,9 +12,15 @@ namespace Microsoft.AspNetCore.Builder
         if (methodInfo == null || methodInfo.ReturnType != typeof(Task)) {
           throw new System.Exception("Method cannot be used");
         }
+        
         T endpointInstance = ActivatorUtilities.CreateInstance<T>(app.ServiceProvider);
-        app.MapGet(path, (RequestDelegate)methodInfo
-            .CreateDelegate(typeof(RequestDelegate), endpointInstance));
+        
+        ParameterInfo[] methodParams = mehodInfo!.GetParameters();
+
+        app.MapGet(path, context => (Task)(methodInfo.Invoke(endpointInstance,
+          methodParams
+            .Select(p => p.ParameterType == typeof(HttpContext) 
+              ? context : app.ServiceProvider.GetService(p.ParameterType)).ToArray())));
       }
     }
 }
--- Platform/WeatherMiddleware.cs
+++ Platform/WeatherMiddleware.cs
@@ -4,14 +4,14 @@ namespace Platform
 {
     public class WeatherMiddleware {
       private RequestDelegate next;
-      private IResponseFormatter formatter;
+      //private IResponseFormatter formatter;
 
       public WeatherMiddleware(RequestDelegate nextDelegate, IResponseFormatter respFormatter){
         next = nextDelegate;
-        formatter = respFormatter;
+        //formatter = respFormatter;
       }
 
-      public async Task Invoke(HttpContext context) {
+      public async Task Invoke(HttpContext context, IResponseFormatter formatter) {
         if (context.Request.Path == "/middleware/class"){
           await formatter.Format(context, "Middleware Class: It is raining in London");
         } else {
