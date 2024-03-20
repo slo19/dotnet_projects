d3e920d7de6b92e6f765b0be3c628073a3bfd0ee
saulo-miranda <saulo.miranda@ufv.br>
2024-03-19 22:28:00 -0300

    unbound types

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,25 +1,30 @@
 //using Platform;
-using Platform.Services;
+//using Platform.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IResponseFormatter, TextResponseFormatter>();
-builder.Services.AddScoped<IResponseFormatter, HtmlResponseFormatter>();
-builder.Services.AddScoped<IResponseFormatter, GuidService>();
+builder.Services.AddSingleton(typeof(ICollection<>), typeof(List<>));
 
 var app = builder.Build();
 
-app.MapGet("single", async context => {
-    IResponseFormatter formatter = context.RequestServices
-      .GetRequiredService<IResponseFormatter>();
-    await formatter.Format(context, "Single service");
-    } 
+app.MapGet("string", async context => {
+    ICollection<string> collection = context.RequestServices
+      .GetRequiredService<ICollection<string>>();
+    collection.Add($"Request: { DateTime.Now.ToLongTimeString() }");
+    foreach (string str in collection) {
+      await context.Response.WriteAsync($"String: {str}\n");
+    }
+  } 
 );
 
-app.MapGet("/", async context => {
-  IResponseFormatter formatter = context.RequestServices
-    .GetServices<IResponseFormatter>().First(f => f.RichOutput);
-  await formatter.Format(context, "Multiple services");
+app.MapGet("int", async context => {
+  ICollection<int> collection = context.RequestServices
+    .GetRequiredService<ICollection<int>>();
+  collection.Add(collection.Count() + 1);
+  foreach (int item in collection)
+  {
+    await context.Response.WriteAsync($"Int: {item}\n");
+  }
 });
 
 app.Run();
