d145dfc98dc4abd5f8f6bef3e5bfe8b0599f1de5
saulo-miranda <saulo.miranda@ufv.br>
2024-03-19 23:05:40 -0300

    setup

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,30 +1,9 @@
-//using Platform;
-//using Platform.Services;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSingleton(typeof(ICollection<>), typeof(List<>));
 
 var app = builder.Build();
 
-app.MapGet("string", async context => {
-    ICollection<string> collection = context.RequestServices
-      .GetRequiredService<ICollection<string>>();
-    collection.Add($"Request: { DateTime.Now.ToLongTimeString() }");
-    foreach (string str in collection) {
-      await context.Response.WriteAsync($"String: {str}\n");
-    }
-  } 
-);
-
-app.MapGet("int", async context => {
-  ICollection<int> collection = context.RequestServices
-    .GetRequiredService<ICollection<int>>();
-  collection.Add(collection.Count() + 1);
-  foreach (int item in collection)
-  {
-    await context.Response.WriteAsync($"Int: {item}\n");
-  }
+app.MapGet("/", async context => {    
+  await context.Response.WriteAsync("Hello World!");    
 });
 
 app.Run();
