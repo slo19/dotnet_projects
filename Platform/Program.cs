db8c49a4cedd8487f613ef11f6473ab0885c2bf6
saulo-miranda <saulo.miranda@ufv.br>
2024-03-21 23:03:28 -0300

    using loggerfactory to generate log messages with a defined category

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -4,6 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+var logger = app.Services
+      .GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+
+logger.LogDebug("Pipeline configuration starting");
+
 app.MapGet("population/{city?}", Population.Endpoint);
+
+logger.LogDebug("Pipeline configuration complete");
 
 app.Run();
