0837a9650d58e78b33957678607fce3ed210a24a
saulo-miranda <saulo.miranda@ufv.br>
2024-03-21 22:57:00 -0300

    added logger

--- Platform/Population.cs
+++ Platform/Population.cs
@@ -1,25 +1,36 @@
-namespace Platform {
-  public class Population {
-    public static async Task Endpoint(HttpContext context) {
-      string city = context.Request.RouteValues["city"] as string ?? "london";
-      int? pop = null;
-      switch (city.ToLower()) {
-        case "london":
-          pop = 8_136_000;
-          break;
-        case "paris":
-          pop = 2_141_000;
-          break;
-        case "monaco":
-          pop = 39_000;
-          break;
-      }
-      if(pop.HasValue) {
-        await context.Response
-          .WriteAsync($"City: {city}, Population: {pop}");
-      } else {
-        context.Response.StatusCode = StatusCodes.Status404NotFound;
-      }
+namespace Platform
+{
+    public class Population
+    {
+
+        public static async Task Endpoint(HttpContext context,
+            ILogger<Population> logger)
+        {
+            logger.LogDebug($"Started processing for {context.Request.Path}");
+            string city = context.Request.RouteValues["city"] as string ?? "london";
+            int? pop = null;
+            switch (city.ToLower())
+            {
+                case "london":
+                    pop = 8_136_000;
+                    break;
+                case "paris":
+                    pop = 2_141_000;
+                    break;
+                case "monaco":
+                    pop = 39_000;
+                    break;
+            }
+            if (pop.HasValue)
+            {
+                await context.Response
+                  .WriteAsync($"City: {city}, Population: {pop}");
+            }
+            else
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+            }
+            logger.LogDebug($"Finished processing for {context.Request.Path}");
+        }
     }
-  }
 }
--- Platform/Program.cs
+++ Platform/Program.cs
@@ -2,34 +2,8 @@ using Platform;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var servicesConfig = builder.Configuration;
-builder.Services.Configure<MessageOptions>(servicesConfig.GetSection("Location"));
-
-var servicesEnv = builder.Environment;
-// use environment to set up services
-
 var app = builder.Build();
 
-var pipelineConfig = app.Configuration;
-
-var pipelineEnv = app.Environment;
-// use environment to set up pipeline
-
-app.UseMiddleware<LocationMiddleware>();
-
-app.MapGet("config", async (HttpContext context,
-      IConfiguration config, IWebHostEnvironment env) =>
-{
-    string defaultDebug = config["Logging:LogLevel:Default"];
-    await context.Response
-      .WriteAsync($"The config setting is: {defaultDebug}");
-    string environ = config["ASPNETCORE_ENVIRONMENT"];
-    await context.Response.WriteAsync($"\nThe env setting is: {env.EnvironmentName}");
-});
-
-app.MapGet("/", async context =>
-{
-    await context.Response.WriteAsync("Hello World!");
-});
+app.MapGet("population/{city?}", Population.Endpoint);
 
 app.Run();
