ba4798ecea5abb9e1d3432dddc7358257dcf0447
saulo-miranda <saulo.miranda@ufv.br>
2024-03-22 22:55:24 -0300

    add logging

--- Platform/Population.cs
+++ Platform/Population.cs
@@ -1,12 +1,13 @@
 namespace Platform
 {
-    public class Population
+    public partial class Population
     {
 
         public static async Task Endpoint(HttpContext context,
             ILogger<Population> logger)
         {
-            logger.LogDebug($"Started processing for {context.Request.Path}");
+            //logger.LogDebug($"Started processing for {context.Request.Path}");
+            StartingResponse(logger, context.Request.Path);
             string city = context.Request.RouteValues["city"] as string ?? "london";
             int? pop = null;
             switch (city.ToLower())
@@ -32,5 +33,7 @@ namespace Platform
             }
             logger.LogDebug($"Finished processing for {context.Request.Path}");
         }
+        [LoggerMessage(0, LogLevel.Debug, "Starting response for {path}")]
+        public static partial void StartingResponse(ILogger logger, string path);
     }
 }
