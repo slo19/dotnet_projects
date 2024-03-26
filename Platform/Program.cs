42111c8c998bab64129daf19d407114e98496f2d
saulo-miranda <saulo.miranda@ufv.br>
2024-03-26 19:17:35 -0300

    logging http request details

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,16 +1,25 @@
 using Platform;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpLogging(opts =>
+{
+    opts.LoggingFields = HttpLoggingFields.RequestMethod |
+      HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
+});
+
 var app = builder.Build();
 
-var logger = app.Services
-      .GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+app.UseHttpLogging();
 
-logger.LogDebug("Pipeline configuration starting");
+//var logger = app.Services
+//      .GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+
+//logger.LogDebug("Pipeline configuration starting");
 
 app.MapGet("population/{city?}", Population.Endpoint);
 
-logger.LogDebug("Pipeline configuration complete");
+//logger.LogDebug("Pipeline configuration complete");
 
 app.Run();
